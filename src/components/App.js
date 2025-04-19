e9a490409c82c313a607ae7841997284e123c699
Ryan <ryan.kiprop@student.moringaschool.com>
2025-04-19 14:04:58 +0300

    reacting

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,37 @@
-import React from "react";
-import blogData from "../data/blog";
 
-console.log(blogData);
+import React from "react";
+import Header from "./Header";
+import About from "./About";
+import ArticleList from "./ArticleList";
+import logo from "../assets/logo"; 
+
+const posts = [
+  {
+    id: 1,
+    title: "Components 101",
+    date: "December 15, 2020",
+    preview: "Setting up the building blocks of your site",
+  },
+  {
+    id: 2,
+    title: "React Data Flow",
+    date: "December 11, 2020",
+    preview: "Passing props is never passé",
+  },
+  {
+    id: 3,
+    title: "Function Components vs Class Components",
+    date: "December 10, 2020",
+    preview: "React, meet OOJS.",
+  },
+];
 
 function App() {
   return (
     <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+      <Header name="Underreacted" />
+      <About image={logo} about="A blog about React and frontend development." />
+      <ArticleList posts={posts} />
     </div>
   );
 }
